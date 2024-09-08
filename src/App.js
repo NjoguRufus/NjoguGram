08ee5de4f60088021a65bc9afbb491142fdc1d7a
NjoguRufus <rsgxliv@gmail.com>
2024-09-08 16:23:07 +0300

    rectify

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
-import Home from './Pages.js/Home';
-import Explore from './Pages.js/Explore';
-import Reels from './components/Reels';
-import Profile from './Pages.js/Profile';
+import Home from './Pages/Home'; // Note the capital 'P'
+import Explore from './Pages/Explore'; // Note the capital 'P'
+import Reels from './Pages/Reels'; // Note the capital 'P'
+import Profile from './Pages/Profile'; // Note the capital 'P'
 
 function App() {
   return (
