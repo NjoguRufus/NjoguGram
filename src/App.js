b8914918a40419b9cafc641eaa9c1fd451a037a4
NjoguRufus <rsgxliv@gmail.com>
2024-09-08 15:38:21 +0300

    corrected

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './Pages.js/Home';
 import Explore from './Pages.js/Explore';
 import Reels from './components/Reels';
 import Profile from './Pages.js/Profile';
 
-
 function App() {
   return (
     <Router>
       <Header />
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/explore" component={Explore} />
-        <Route path="/reels" component={Reels} />
-        <Route path="/profile" component={Profile} />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="/reels" element={<Reels />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </Router>
   );
 }
